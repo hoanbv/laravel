bfbd359f05bc824c98a676c775ca8cfaaf7c8886
hoan <steroinnn@gmail.com>
2018-07-09 21:04:01 +0700

    save laravel buoi 2

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//Route::get('/user', function ()
+//{
+//    return " day la user";
+//});
+
+Route::match(['put', 'post'], '/', function () {
+    echo ' test match';
 });
+Route::any('foo', function () {
+    echo ' test foo';
+});
+Route::get('user/{id}', function ($id){
+  echo " test id".$id;
+});
+
+Route::get('post/{post}', function ($postId){
+    echo ' post'.$postId;
+})->where('post', '[A-Za-z]+');
+
+Route::get('author/{name?}', function ($name = 'Jhon'){
+    return $name;
+});
+Route::view('name','route',['name'=>'laravel']);
